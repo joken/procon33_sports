264b545046c63b4b0ed2ffba77c53e867e1c55d7
Gisuperu <gisubright@gmail.com>
2022-10-06 10:55:46 +0900

    analyze.py maked

---
+++ analyze/analyze.py
@@ -0,0 +1,15 @@
+import json
+from .. import main
+
+def buckup(match, stage, analyzed_data):
+    print()
+
+def analyze(match, stage, analyzed_data):
+    
+    
+    buckup(match, stage, analyzed_data)
+    return analyzed_data
+
+
+if __name__ == '__main__':
+    analyze()
